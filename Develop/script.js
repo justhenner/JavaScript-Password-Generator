08d1c85a362a0d257b061c12357358144277b5b8
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2022-12-22 23:09:19 -0500

    global variables established

---
+++ Develop/script.js
@@ -0,0 +1,30 @@
+// Assignment code here
+var generateBtn = document.querySelector("#generate");
+
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+
+var numbers = "1234567890";
+
+var symbols = "!#$%&'()*+,-.?:;<=>?@[]\]^_`{|}~";
+
+function generatePassword()
+  console.log(randomNumber)
+
+
+
+  return randomNumber
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
